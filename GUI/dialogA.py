21ca1ebd711add59f7b477e29eda249e4c068a58
energyXperts.NET <energyxperts@users.sourceforge.net>
2008-04-17 10:36:30 +0000

    git-svn-id: https://einstein.svn.sourceforge.net/svnroot/einstein/trunk@44 655c6d59-fa48-0410-8d56-e6d7b3dfb13a

---
+++ GUI/dialogA.py
@@ -0,0 +1,64 @@
+#Boa:Dialog:DialogA
+
+import wx
+
+def create(parent):
+    return DialogA(parent)
+
+[wxID_DIALOGA, wxID_DIALOGABUTTONCANCEL, wxID_DIALOGABUTTONOK, 
+ wxID_DIALOGASTDESCRIPTION, wxID_DIALOGASTDIALOG1, wxID_DIALOGATCDESCRIPTION, 
+ wxID_DIALOGATCSHORTNAME, 
+] = [wx.NewId() for _init_ctrls in range(7)]
+
+class DialogA(wx.Dialog):
+    def _init_ctrls(self, prnt):
+        # generated method, don't edit
+        wx.Dialog.__init__(self, id=wxID_DIALOGA, name='DialogA', parent=prnt,
+              pos=wx.Point(433, 283), size=wx.Size(400, 188),
+              style=wx.DEFAULT_DIALOG_STYLE, title='New alternative proposal')
+        self.SetClientSize(wx.Size(392, 154))
+
+        self.stDialog1 = wx.StaticText(id=wxID_DIALOGASTDIALOG1,
+              label='shortname:', name='stDialog1', parent=self,
+              pos=wx.Point(24, 16), size=wx.Size(56, 13), style=0)
+        self.stDialog1.Center(wx.HORIZONTAL)
+
+        self.buttonOK = wx.Button(id=wxID_DIALOGABUTTONOK, label='OK',
+              name='buttonOK', parent=self, pos=wx.Point(176, 112),
+              size=wx.Size(91, 23), style=0)
+        self.buttonOK.Bind(wx.EVT_BUTTON, self.OnButtonOKButton,
+              id=wxID_DIALOGABUTTONOK)
+
+        self.buttonCancel = wx.Button(id=wxID_DIALOGABUTTONCANCEL,
+              label='cancel', name='buttonCancel', parent=self,
+              pos=wx.Point(280, 112), size=wx.Size(91, 23), style=0)
+        self.buttonCancel.Bind(wx.EVT_BUTTON, self.OnButtonCancelButton,
+              id=wxID_DIALOGABUTTONCANCEL)
+
+        self.stDescription = wx.StaticText(id=wxID_DIALOGASTDESCRIPTION,
+              label='description:', name='stDescription', parent=self,
+              pos=wx.Point(24, 40), size=wx.Size(57, 13), style=0)
+        self.stDescription.Center(wx.HORIZONTAL)
+
+        self.tcShortName = wx.TextCtrl(id=wxID_DIALOGATCSHORTNAME,
+              name='tcShortName', parent=self, pos=wx.Point(96, 16),
+              size=wx.Size(280, 16), style=0, value='new alternative')
+
+        self.tcDescription = wx.TextCtrl(id=wxID_DIALOGATCDESCRIPTION,
+              name='tcDescription', parent=self, pos=wx.Point(96, 40),
+              size=wx.Size(280, 56), style=0,
+              value='describe briefly the new alternative')
+
+    def __init__(self, parent):
+        self._init_ctrls(parent)
+        self.prnt = parent
+        self.tcShortName.SetValue(parent.shortName)
+        self.tcDescription.SetValue(parent.description)
+
+    def OnButtonOKButton(self, event):
+        self.prnt.shortName = self.tcShortName.GetValue()
+        self.prnt.description = self.tcDescription.GetValue()
+        self.EndModal(wx.ID_OK)
+
+    def OnButtonCancelButton(self, event):
+        self.EndModal(wx.Cancel)
---
+++ GUI/dialogOK.py
@@ -0,0 +1,46 @@
+#Boa:Dialog:DialogOK
+
+import wx
+
+def create(parent):
+    return DialogOK(parent)
+
+[wxID_DIALOGOK, wxID_DIALOGOKBUTTONCANCEL, wxID_DIALOGOKBUTTONOK, 
+ wxID_DIALOGOKSTDIALOG, 
+] = [wx.NewId() for _init_ctrls in range(4)]
+
+class DialogOK(wx.Dialog):
+    def _init_ctrls(self, prnt):
+        # generated method, don't edit
+        wx.Dialog.__init__(self, id=wxID_DIALOGOK, name="dialogA", parent=prnt,
+              pos=wx.Point(433, 286), size=wx.Size(400, 171),
+              style=wx.DEFAULT_DIALOG_STYLE, title=self.title)
+        self.SetClientSize(wx.Size(392, 137))
+
+        self.stDialog = wx.StaticText(id=wxID_DIALOGOKSTDIALOG,
+              label=self.message, name='stDialog', parent=self,
+              pos=wx.Point(24, 16), size=wx.Size(344, 64), style=0)
+        self.stDialog.Center(wx.HORIZONTAL)
+
+        self.buttonOK = wx.Button(id=wxID_DIALOGOKBUTTONOK, label='OK',
+              name='buttonOK', parent=self, pos=wx.Point(176, 96),
+              size=wx.Size(91, 23), style=0)
+        self.buttonOK.Bind(wx.EVT_BUTTON, self.OnButtonOKButton,
+              id=wxID_DIALOGOKBUTTONOK)
+
+        self.buttonCancel = wx.Button(id=wxID_DIALOGOKBUTTONCANCEL,
+              label='cancel', name='buttonCancel', parent=self,
+              pos=wx.Point(280, 96), size=wx.Size(91, 23), style=0)
+        self.buttonCancel.Bind(wx.EVT_BUTTON, self.OnButtonCancelButton,
+              id=wxID_DIALOGOKBUTTONCANCEL)
+
+    def __init__(self, parent, title, message):
+        self.message = message
+        self.title = title
+        self._init_ctrls(parent)
+
+    def OnButtonOKButton(self, event):
+        self.EndModal(wx.ID_OK)
+
+    def OnButtonCancelButton(self, event):
+        self.EndModal(wx.ID_CANCEL)
