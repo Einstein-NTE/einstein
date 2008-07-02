d0c86771f6a3f03a23eb00bbdb5b6942a922fc12
energyXperts.NET <energyxperts@users.sourceforge.net>
2008-07-02 12:29:06 +0000

    git-svn-id: https://einstein.svn.sourceforge.net/svnroot/einstein/trunk@150 655c6d59-fa48-0410-8d56-e6d7b3dfb13a

--- modules/moduleHR.py
+++ modules/moduleHR.py
@@ -15,14 +15,16 @@
 #
 #==============================================================================
 #
-#   Version No.: 0.01
+#   Version No.: 0.03
 #   Created by:         Hans Schweiger  10/06/2008
 #   Last revised by:
 #                       Hans Schweiger  23/06/2008
+#                       Hans Schweiger  02/07/2008
 #
 #   Changes to previous version:
 #
 #   23/06/2008: HS  Query of fluids and fuels used in export XML
+#   02/07/2008: HS  Function getHXData added -> obtains Fluid ID's in HX
 #   
 #------------------------------------------------------------------------------     
 #   (C) copyleft energyXperts.BCN (E4-Experts SL), Barcelona, Spain 2008
@@ -45,6 +47,8 @@ from einstein.modules.interfaces import *
 import einstein.modules.matPanel as mP
 from einstein.modules.constants import *
 from einstein.modules.exportdata import *
+
+from einstein.modules.messageLogger import *
 
 class ModuleHR(object):
 
@@ -93,6 +97,83 @@ class ModuleHR(object):
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+    def getHXData(self):
+#------------------------------------------------------------------------------
+#       obtains the data of the HX fluids
+#------------------------------------------------------------------------------
+
+        hxes = Status.prj.getHXes()
+        equipments = Status.prj.getEquipments()
+        pipes = Status.prj.getPipes()
+        processes = Status.prj.getProcesses()
+        whees = Status.prj.getWHEEs()
+
+        for hx in hxes:
+            source = hx.HXSource
+            sink = hx.HXSink
+            sourceOK = False
+            sinkOK = False
+
+            print "----------------------------------------------"
+            print "ModuleHR (getHXData): checking HX %s with Source= %s and Sink= %s"%\
+                  (hx.HXName,hx.HXSource,hx.HXSink)
+
+            for eq in equipments:
+                if (sourceOK==True and sinkOK==True): break
+
+                print "Equipment %s: "%eq.Equipment
+                if source == eq.Equipment:
+                    hx.FluidIDSource = None
+                    sourceOK = True
+                    
+                if sink == eq.Equipment:
+                    hx.FluidIDSink = None
+                    sinkOK = True
+
+                print "sourceID/sinkID: %s,%s"%(hx.FluidIDSource,hx.FluidIDSink)
+            
+            for pipe in pipes:
+                if (sourceOK==True and sinkOK==True): break
+                print "Pipe %s: "%pipe.Pipeduct
+                if source == pipe.Pipeduct:
+                    hx.FluidIDSource = pipe.HeatDistMedium
+                    sourceOK = True
+
+                if sink == pipe.Pipeduct:
+                    hx.FluidIDSink = pipe.HeatDistMedium
+                    sinkOK = True
+
+                print "sourceID/sinkID: %s,%s"%(hx.FluidIDSource,hx.FluidIDSink)
+            
+            for process in processes:
+                if (sourceOK==True and sinkOK==True): break
+                print "process %s: "%process.Process
+                if source == process.Process:
+                    hx.FluidIDSource = process.ProcMedOut
+                    sourceOK = True
+                    
+                if sink == process.Process:
+                    hx.FluidIDSink = process.ProcMedDBFluid_id
+                    sinkOK = True
+
+                print "sourceID/sinkID: %s,%s"%(hx.FluidIDSource,hx.FluidIDSink)
+                    
+            for whee in whees:                         #WHEE can only be source, not sink !!!
+                if (sourceOK==True and sinkOK==True): break
+                print "whee %s: "%whee.WHEEName
+                if source == whee.WHEEName:
+                    hx.FluidIDSource = whee.WHEEMedium
+                    sourceOK = True
+
+                print "sourceID/sinkID: %s,%s"%(hx.FluidIDSource,hx.FluidIDSink)
+
+            if sinkOK == False:
+                logWarning("ModuleHR (getHXData): couldn't find a sink ID for HX %s"%hx.HXName)
+                
+            if sourceOK == False:
+                logWarning("ModuleHR (getHXData): couldn't find a source ID for HX %s"%hx.HXName)
+                
+#------------------------------------------------------------------------------
     def runHRModule(self):
 #------------------------------------------------------------------------------
 #       runs the calculations on the external HR module
@@ -100,6 +181,8 @@ class ModuleHR(object):
 
         if Status.schedules.outOfDate == True:
             Status.schedules.create()   #creates the process and equipment schedules
+
+        self.getHXData()
 
         (fluidIDs,fuelIDs) = Status.prj.getFluidAndFuelList()                         
         ex = ExportDataHR(pid=Status.PId, ano=Status.ANo,fuels=fuelIDs, fluids=fluidIDs)
