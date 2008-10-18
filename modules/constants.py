fe42506f33051c9359bd3af9c71d560961de8f48
energyXperts.NET <energyxperts@users.sourceforge.net>
2008-10-18 09:05:12 +0000

    git-svn-id: https://einstein.svn.sourceforge.net/svnroot/einstein/trunk@263 655c6d59-fa48-0410-8d56-e6d7b3dfb13a

--- modules/constants.py
+++ modules/constants.py
@@ -46,7 +46,7 @@
 #============================================================================== 
 
 #------------------------------------------------------------------------------		
-VERSION = "V1.0 Revision No. 259" #Number of upload in sourceforge
+VERSION = "V1.0 Revision No. 263" #Number of upload in sourceforge
 #------------------------------------------------------------------------------		
 DEBUG = "OFF"   #Set to:
 DEBUGMODES = ["OFF","BASIC","MAIN","ALL"]
