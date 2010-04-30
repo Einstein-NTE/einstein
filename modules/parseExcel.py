e23df94a27d56048493b452508bbba653cd38263
Andre Rattinger <Rattinger@ntesystems.at>
2010-04-30 15:46:31 +0200

    added functions splitExcelColumns and getExcelLists

--- modules/parseExcel.py
+++ modules/parseExcel.py
@@ -51,6 +51,90 @@ class parseExcel(parseSpreadsheet):
         xlWb = xlApp.Workbooks.Open(filepath)
         return xlApp, xlWb
     
+    def splitExcelColumns(self,nr_of_elements, columns, parsed_list,dict,Questionnaire_id,createDictionary,db_table):
+        """
+        Splits columns of the excel import and inserts them into the Database
+        nr_of_elements: Number of Columns that should be inserted into the database (count from left)
+        columns: Existing Columns 
+        parsed_list: Parsed list from the Excel Worksheet
+        dict: additional Dictionary that should be included
+        createDictionary: Function that creates the Database Dictionary from the input list
+        db_table: pSQL Database Table
+        Example Usage: 
+        splitExcelColumns(4, 6, QFuel, Questionnaire_ID, createQFuelDictionary,md.qfuel)
+        """
+        list = []
+        for i in xrange(nr_of_elements):
+            for j in xrange(0+i,len(parsed_list),columns):
+                list.append(parsed_list[j])
+            Dict = createDictionary(list)
+            if Questionnaire_id != "":
+                Dict['Questionnaire_id']= Questionnaire_id
+            Dict.update(dict)
+            db_table.insert(Dict)
+            list = []
+    
+    def getExcelLists(self, sheetnames, xlWb): 
+        sht = xlWb.Worksheets(sheetnames[0])
+        
+        Q1=tupleToList(sht.Range("Q1_GeneralData"))    
+        Q1+=tupleToList(sht.Range("Q1_StatisticalData"))
+        Q1+=tupleToList(sht.Range("Q1_Operation"))
+        
+        QProduct =tupleToList(sht.Range("Q1_Products"))
+        
+        sht = xlWb.Worksheets(sheetnames[1])
+        
+        Q1+=tupleToList(sht.Range("Q1_Percent"))
+        QProduct+=tupleToList(sht.Range("Q2_Products"))
+        
+        Q2 = tupleToList(sht.Range("Q2_EnergyConsumption"))
+        Q2 += tupleToList(sht.Range("Q2_ElectricityConsumption"))
+        Q2 += tupleToList(sht.Range("Q2_EnergyConsumptionProduct"))
+        QFuel = tupleToList(sht.Range("Q2_EnergyConsumption"))
+        
+        sht = xlWb.Worksheets(sheetnames[2])
+        Q3 = tupleToList(sht.Range("Q3_ProcessData"))
+        Q3 += tupleToList(sht.Range("Q3_WasteHeat"))
+        Q3 += tupleToList(sht.Range("Q3_Schedule")) 
+        Q3 += tupleToList(sht.Range("Q3_DataOfExistingHCSupply")) 
+        
+        sht= xlWb.Worksheets(sheetnames[3])
+        Q3+= tupleToList(sht.Range("Q3_ScheduleTolerance"))
+        Q3+= tupleToList(sht.Range("Q3_OperationCycle"))
+        
+        sht = xlWb.Worksheets(sheetnames[8])
+        QRenewables = tupleToList(sht.Range("Q7_Interest"))
+        QRenewables += tupleToList(sht.Range("Q7_REReason"))
+        QRenewables += tupleToList(sht.Range("Q7_Others"))
+        QRenewables += tupleToList(sht.Range("Q7_Latitude"))
+        QRenewables += tupleToList(sht.Range("Q7_Biomass"))
+        
+        QSurf = tupleToList(sht.Range("Q7_Area"))
+        QSurf += tupleToList(sht.Range("Q7_Roof"))
+        
+        sht = xlWb.Worksheets(sheetnames[3])
+    
+        QProfiles = []
+        QProcNames = tupleToList(sht.Range("Q3A_ProcessName"))
+        
+        for i in xrange(3):
+            QProfil = tupleToList(sht.Range("Q3A_Profiles_"+ str(i+1)))
+            QProfil.append(QProcNames[i*3])
+            QProfiles.append(QProfil)
+    
+            
+        QIntervals  = tupleToList(sht.Range("Q3A_StartTime_1"))
+        QIntervals += tupleToList(sht.Range("Q3A_StartTime_2"))
+        QIntervals += tupleToList(sht.Range("Q3A_StartTime_3"))
+        QIntervals += tupleToList(sht.Range("Q3A_EndTime_1"))
+        QIntervals += tupleToList(sht.Range("Q3A_EndTime_2"))
+        QIntervals += tupleToList(sht.Range("Q3A_EndTime_3"))
+        
+        
+        return Q1, Q2, QProduct, QFuel, Q3 , QRenewables, QSurf, QProfiles, QIntervals
+
+    
     def parse(self):
         self.__xlApp, self.__xlWb = self.openExcelDispatch(self.__filepath)
         __md = self.__connectToDB()
